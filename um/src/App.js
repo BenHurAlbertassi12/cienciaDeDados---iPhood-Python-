d36a785bb7a51f205b02f884bb528be4b3ab8a03
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-07 17:02:40 -0300

    Exemplo 1

--- um/src/App.js
+++ um/src/App.js
@@ -1,39 +1,37 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
-export default class App extends Component{
+export default class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       name: 'BenHur Albertassi',
       counter: 0
     };
 
-    this.handlePClick = this.handlePClick.bind(this)
+    this.handlePClick = this.handlePClick.bind(this);
   }
-  handleAClick = (event) => { 
-    event.preventDefault()
+
+  handleAClick = (event) => {
+    event.preventDefault();
     const { counter } = this.state;
-      this.setState({ counter: counter + 1 })
-    
+    this.setState({ counter: counter + 1 });
   }
 
   handlePClick() {
-    this.setState({name: 'Albertassi'})
+    this.setState({ name: 'Albertassi' });
   }
+
   render() {
-
-    const { name, counter } = this.state
-
+    const { name, counter } = this.state;
 
     return (
       <div>
-      <h1 onClick={()=>this.handlePClick()}>
-        { name }
-      </h1>
-        <h2 onClick={() => this.handleAClick()}>{ counter }</h2>
+        <h1 onClick={this.handlePClick}>
+          {name}
+        </h1>
+        <h2 onClick={this.handleAClick}>{counter}</h2>
       </div>
-  
-    )
+    );
   }
 }
