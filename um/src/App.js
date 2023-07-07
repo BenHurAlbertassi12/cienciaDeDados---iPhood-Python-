6beaf49e8e3f1baa6b1596af06b189960f609902
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-07 16:47:54 -0300

    exemplo 1

--- um/src/App.js
+++ um/src/App.js
@@ -1,10 +1,30 @@
 import { Component } from "react";
 
 export default class App extends Component{
-  render() {
-    return (
-      <h1>Ola</h1>
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      name: 'BenHur Albertassi'
+    };
+
+    this.handlePClick = this.handlePClick.bind(this)
+  }
+
+  handlePClick() {
+    const { name } = this.state
+    console.log(`<h1> Clicado</h1> ${name}`);
+  }
+  render() {
+
+    const { name } = this.state
+
+
+    return (
+      <h1 onClick={()=>this.handlePClick()}>
+        { name }
+    </h1>
+  
     )
   }
 }
