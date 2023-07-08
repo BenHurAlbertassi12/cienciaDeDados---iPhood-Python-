27a1074bb161b73aeb0e4b36ac8e6ff8072a957f
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-08 14:02:06 -0300

    Exemplo 1

--- um/src/App.js
+++ um/src/App.js
@@ -5,39 +5,35 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      post: []
+      counter: 0,
+      post: [{
+        id: 1,
+        title: 'titulo 1',
+        body: 'body 1'
+      },
+        {
+          id: 2,
+          title: 'titulo 2',
+          body: 'body 2'
+        },
+        {
+          id: 3,
+          title: 'titulo 3',
+          body: 'body 3'
+        }]
     };
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        post: [
-          {
-            id: 1,
-            title: 'titulo 1',
-            body: 'body 1'
-          },
-          {
-            id: 2,
-            title: 'titulo 2',
-            body: 'body 2'
-          },
-          {
-            id: 3,
-            title: 'titulo 3',
-            body: 'body 3'
-          }
-        ]
-      });
-    }, 5000);
+
   }
 
   render() {
-    const { post } = this.state;
+    const { post, counter } = this.state;
 
     return (
       <div>
+        <h1>{ counter }</h1>
         {post.map(post => (
           <div key={post.id}>
             <h1>{post.title}</h1>
