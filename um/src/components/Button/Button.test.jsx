37e03674ac16af580c7c9b1da4140f057253622b
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-11 23:31:19 -0300

    Teste 2

--- um/src/components/Button/Button.test.jsx
+++ um/src/components/Button/Button.test.jsx
@@ -1,11 +1,22 @@
 import { render, screen } from "@testing-library/react"
 import ButtonLoadMore from "./ButtonLoadMore"
+import userEvent from "@testing-library/user-event";
 
 describe('<buttonLoadMore />', () => {
     it('Should render the button with the text "LoadMorePosts"', () => {
         render(<ButtonLoadMore text='LoadMorePosts' />);
-
+        
         const button = screen.getByRole('button', { name: /LoadMorePosts/i })
         expect(button).toBeInTheDocument()
+    })
+    it('Should call function on button click', () => {
+        const fn = jest.fn()
+        render(<ButtonLoadMore text='LoadMorePosts' onClick={fn} />);
+        
+        const button = screen.getByRole('button', { name: /LoadMorePosts/i })
+
+        userEvent.click(button)
+
+        expect(fn).toHaveBeenCalledTimes(1)
     })
 })
