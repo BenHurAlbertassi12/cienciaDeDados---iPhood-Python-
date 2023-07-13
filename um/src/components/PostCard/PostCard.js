dda993fd321bf746ca30d34f6f786e2d3de7b656
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-13 13:21:35 -0300

    Props

--- um/src/components/PostCard/PostCard.js
+++ um/src/components/PostCard/PostCard.js
@@ -1,11 +1,19 @@
-export const PostCard = ({ cover, title, body, id }) => {
+import props from 'prop-types';
+
+export const PostCard = ({ cover, title, body }) => {
   return (
-    <div className='post'>
+    <div className="post">
       <img src={cover} alt={title}></img>
-      <div className='post-content'>
+      <div className="post-content">
         <h1>{title}</h1>
         <p>{body}</p>
       </div>
     </div>
   );
-}; 
+};
+
+PostCard.propTypes = {
+  title: props.string.isRequired,
+  cover: props.string.isRequired,
+  body: props.string.isRequired,
+};
