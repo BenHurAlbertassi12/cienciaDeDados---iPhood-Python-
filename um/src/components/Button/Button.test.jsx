c959b21219f5b39e2c703df0160af2627e282da5
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-13 13:08:50 -0300

    ButtonLint

--- um/src/components/Button/Button.test.jsx
+++ um/src/components/Button/Button.test.jsx
@@ -1,38 +1,36 @@
-import { render, screen } from "@testing-library/react"
-import ButtonLoadMore from "./ButtonLoadMore"
-import userEvent from "@testing-library/user-event";
+import { render, screen } from '@testing-library/react';
+import ButtonLoadMore from './ButtonLoadMore';
+import userEvent from '@testing-library/user-event';
 
 describe('<buttonLoadMore />', () => {
-    it('Should render the button with the text "LoadMorePosts"', () => {
-        render(<ButtonLoadMore text='LoadMorePosts' />);
-        
-        const button = screen.getByRole('button', { name: /LoadMorePosts/i })
-        expect(button).toBeInTheDocument()
-    })
-    it('Should call function on button click', () => {
-        const fn = jest.fn()
-        render(<ButtonLoadMore text='LoadMorePosts' onClick={fn} />);
-        
-        const button = screen.getByRole('button', { name: /LoadMorePosts/i })
+  it('Should render the button with the text "LoadMorePosts"', () => {
+    render(<ButtonLoadMore text="LoadMorePosts" />);
 
-        userEvent.click(button)
+    const button = screen.getByRole('button', { name: /LoadMorePosts/i });
+    expect(button).toBeInTheDocument();
+  });
+  it('Should call function on button click', () => {
+    const fn = jest.fn();
+    render(<ButtonLoadMore text="LoadMorePosts" onClick={fn} />);
 
-        expect(fn).toHaveBeenCalledTimes(1)
-    })
-    it('Should be disabled when disabled is true', () => {
-       render(<ButtonLoadMore text='LoadMorePosts' disabled={true} />);
-        
-        const button = screen.getByRole('button', { name: /LoadMorePosts/i })
+    const button = screen.getByRole('button', { name: /LoadMorePosts/i });
 
-        expect(button).toBeDisabled()
- 
-    })
-    it('Should be disabled when disabled is false', () => {
-       render(<ButtonLoadMore text='LoadMorePosts' disabled={false} />);
-        
-        const button = screen.getByRole('button', { name: /LoadMorePosts/i })
+    userEvent.click(button);
 
-        expect(button).toBeEnabled()
- 
-    })
-})
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+  it('Should be disabled when disabled is true', () => {
+    render(<ButtonLoadMore text="LoadMorePosts" disabled={true} />);
+
+    const button = screen.getByRole('button', { name: /LoadMorePosts/i });
+
+    expect(button).toBeDisabled();
+  });
+  it('Should be disabled when disabled is false', () => {
+    render(<ButtonLoadMore text="LoadMorePosts" disabled={false} />);
+
+    const button = screen.getByRole('button', { name: /LoadMorePosts/i });
+
+    expect(button).toBeEnabled();
+  });
+});
