3581de7082e7eda8499c02406998eeb7d16d9abe
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-13 13:23:45 -0300

    Posts props

--- um/src/components/Posts/Posts.jsx
+++ um/src/components/Posts/Posts.jsx
@@ -1,15 +1,18 @@
+import p from 'prop-types';
 import { PostCard } from '../PostCard/PostCard';
 
-export const Posts = ({posts}) => (
-  <div className='posts'>
+export const Posts = ({ posts }) => (
+  <div className="posts">
     {posts.map((post) => (
-      <PostCard
-        key={post.id}
-        title={post.title}
-        body={post.body}
-        id={post.id}
-        cover={post.cover}
-      />
+      <PostCard key={post.id} title={post.title} body={post.body} id={post.id} cover={post.cover} />
     ))}
   </div>
 );
+
+Posts.defaultProps = {
+  posts: [],
+};
+
+Posts.propTypes = {
+  posts: p.array,
+};
