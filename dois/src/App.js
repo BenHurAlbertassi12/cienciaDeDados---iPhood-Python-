df825f3d7734236f0a18cf494a062f32fdd4bd8d
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-16 16:14:55 -0300

    useCallBack - React Hook

--- dois/src/App.js
+++ dois/src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
-import { useState } from 'react';
 
-const Button = ({ incrementButton }) => {
+const Button = React.memo(function Button({ incrementButton }) {
   return <button onClick={() => incrementButton(10)}>+</button>;
-};
+});
 
 Button.propTypes = {
   incrementButton: PropTypes.func,
@@ -14,9 +13,9 @@ Button.propTypes = {
 function App() {
   const [counter, setCounter] = useState(0);
 
-  const incrementCounter = (num) => {
-    setCounter(counter + num);
-  };
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num);
+  }, []);
 
   return (
     <div className="App">
