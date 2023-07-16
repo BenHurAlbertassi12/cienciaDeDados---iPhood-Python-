441a9c8050e0ac0be40f337d4fb9c2572d6d295d
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-16 17:53:47 -0300

    fetch

--- dois/src/App.js
+++ dois/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import PropTypes from 'prop-types';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  console.log('pai renderizou');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
+  }, []);
+
   return (
     <div className="App">
-      <h1>oi</h1>
+      {posts.map((post) => {
+        return (
+          <div className="post" key={posts.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
