acac36001e298bef40134c8725c9b810d9c26ec9
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-16 15:57:11 -0300

    userCallBack

--- dois/src/App.js
+++ dois/src/App.js
@@ -1,42 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-const eventFn = () => {
-  console.log('h1 clicado');
+const Button = ({ incrementButton }) => {
+  return <button onClick={() => incrementButton(10)}>+</button>;
+};
+
+Button.propTypes = {
+  incrementButton: PropTypes.func,
 };
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
 
-  // componentDidUpdate - executa toda vez que o component atualiza
-  // useEffect(() => {
-  //   console.log('componentDidUpdate');
-  // });
-
-  // componentDidMount - executa 1x
-  useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-
-    // componentWillUmount - limpeza
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
-  }, []);
-
-  // Com dependência - executa toda vez que a dependência mudar
-  useEffect(() => {
-    console.log('C1:', counter, 'C2:', counter2);
-  }, [counter, counter2]);
+  const incrementCounter = (num) => {
+    setCounter(counter + num);
+  };
 
   return (
     <div className="App">
-      <p>Teste 3</p>
-      <h1>
-        C1: {counter} C2: {counter2}{' '}
-      </h1>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-      <button onClick={() => setCounter2(counter2 + 1)}>+ (2)</button>
+      <p>Teste</p>
+      <h1>C1: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
     </div>
   );
 }
