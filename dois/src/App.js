0de3b6ff7e530760744d9dd6bab899d79efa82ca
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-17 15:58:04 -0300

    useMtHook

--- dois/src/App.js
+++ dois/src/App.js
@@ -1,77 +1,33 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
-import P from 'prop-types';
-import { createContext, useContext, useReducer, useRef } from 'react';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
-// actions.js
-export const actions = {
-  CHANGE_TITLE: 'CHANGE_TITLE',
+const useMyHook = (cb) => {
+  const savedCB = useRef();
+
+  useEffect(() => {
+    savedCB.current = cb;
+  }, [cb]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      savedCB.current();
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 };
 
-// data.js
-export const globalState = {
-  title: 'O título que contexto',
-  body: 'O body do contexto',
-  counter: 0,
-};
+function App() {
+  const [counter, setCounter] = useState(0);
 
-// reducer.js
-export const reducer = (state, action) => {
-  switch (action.type) {
-    case actions.CHANGE_TITLE: {
-      console.log('Mudar título');
-      return { ...state, title: action.payload };
-    }
-  }
-
-  return { ...state };
-};
-
-// AppContext.jsx
-export const Context = createContext();
-export const AppContext = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, globalState);
-
-  const changeTitle = (payload) => {
-    dispatch({ type: actions.CHANGE_TITLE, payload });
-  };
-
-  return (
-    <Context.Provider value={{ state, changeTitle }}>
-      {children}
-    </Context.Provider>
-  );
-};
-
-AppContext.propTypes = {
-  children: P.node,
-};
-
-// H1/index.jsx
-export const H1 = () => {
-  const context = useContext(Context);
-  const inputRef = useRef();
+  useMyHook(() => setCounter((c) => c + 1));
 
   return (
     <>
-      <h1 onClick={() => context.changeTitle(inputRef.current.value)}>
-        {context.state.title}
-      </h1>
-      <input type="text" ref={inputRef} />
+      <h1>Countador: {counter}</h1>
     </>
-  );
-};
-
-// App.jsx
-function App() {
-  return (
-    <AppContext>
-      <div>
-        <H1 />
-      </div>
-    </AppContext>
   );
 }
 
