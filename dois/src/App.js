2dc41e62ce7b7786a184a308b66b7c683b7d13dd
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-17 10:08:06 -0300

    Intital

--- dois/src/App.js
+++ dois/src/App.js
@@ -1,1 +1,12 @@
-export const App = () => {};
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1> Ola </h1>;
+    </div>
+  );
+};
+
+export default App;
