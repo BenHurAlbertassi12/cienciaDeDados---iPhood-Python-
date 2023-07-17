e88f7602abc21d02618074afb225b661dfc67c2d
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-17 14:59:59 -0300

    useReducer - 1

--- dois/src/App.js
+++ dois/src/App.js
@@ -1,15 +1,53 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
+import { useReducer } from 'react';
 import './App.css';
-import { Div } from './components/Div/Div';
-import { AppContext } from './context/App';
+// import { Div } from './components/Div/Div';
+// import { AppContext } from './context/App';
+
+// function App() {
+//   return (
+//     <AppContext>
+//       <Div />
+//     </AppContext>
+//   );
+// }
+
+// export default App;
+
+const globalState = {
+  title: 'o titulo do context',
+  body: 'o corpo do context',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      console.log('Chamou muda');
+      return { ...state, title: 'mudou' };
+    }
+    case 'inverter': {
+      console.log('Chamou inverter');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+  return { ...state };
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <>
+      <h1>{state.title}</h1>
+      <p>{state.body}</p>
+      <p>Counter: {state.counter}</p>
+      <button onClick={() => dispatch({ type: 'muda' })}>muda</button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>invert</button>
+    </>
   );
 }
 
