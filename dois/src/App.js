859a9aa3993d9cd65c72ff09baf1dacc31594a0b
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-16 22:10:24 -0300

    useRef

--- dois/src/App.js
+++ dois/src/App.js
@@ -1,12 +1,14 @@
 import P from 'prop-types';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 
-const Post = ({ post }) => {
-  console.log('Filho renderizou');
+const Post = ({ post, handleClick }) => {
+  // console.log('Filho renderizou');
   return (
     <div key={post.id} className="post">
-      <h1>{post.title}</h1>
+      <h1 style={{ fontSize: '32px' }} onClick={() => handleClick(post.title)}>
+        {post.title}
+      </h1>
       <p>{post.body}</p>
     </div>
   );
@@ -18,13 +20,20 @@ Post.propTypes = {
     title: P.string,
     body: P.string,
   }),
+  handleClick: P.string,
 };
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
+  const input = useRef(null);
+  const contador = useRef(0);
 
-  console.log('Pai renderizou!');
+  const handleClick = (value) => {
+    setValue(value);
+  };
+
+  // console.log('Pai renderizou!');
 
   // Component did mount
   useEffect(() => {
@@ -33,10 +42,21 @@ function App() {
       .then((r) => setPosts(r));
   }, []);
 
+  useEffect(() => {
+    input.current.focus();
+    console.log(input.current);
+  }, [value]);
+
+  useEffect(() => {
+    contador.current++;
+  });
+
   return (
     <div className="App">
+      <h1>Render: {contador.current}X</h1>
       <p>
         <input
+          ref={input}
           type="search"
           value={value}
           onChange={(e) => setValue(e.target.value)}
@@ -46,7 +66,7 @@ function App() {
         return (
           posts.length > 0 &&
           posts.map((post) => {
-            return <Post key={post.id} post={post} />;
+            return <Post key={post.id} post={post} handleClick={handleClick} />;
           })
         );
       }, [posts])}
