07b04f6e846be9be15bc277083f3e39fda53aee0
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-17 10:54:41 -0300

    context

--- dois/src/App.js
+++ dois/src/App.js
@@ -1,11 +1,52 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React, { useContext, useState } from 'react';
 import './App.css';
 
-const App = () => {
+const globalState = {
+  title: 'titulo do contexto',
+  counter: 0,
+  body: 'body do contexto',
+};
+
+const GlobalContext = React.createContext();
+
+const Div = ({ children }) => {
   return (
-    <div className="App">
-      <h1> Ola </h1>;
-    </div>
+    <>
+      <H1 />
+      <BodyComponent />
+    </>
+  );
+};
+
+const H1 = () => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: { title, counter },
+  } = theContext;
+  return (
+    <h1>
+      {title} {counter}
+    </h1>
+  );
+};
+
+const BodyComponent = () => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: { body },
+  } = theContext;
+  return <p>{body}</p>;
+};
+
+const App = () => {
+  const [contextState, setContextState] = useState(globalState);
+
+  return (
+    <GlobalContext.Provider value={{ contextState, setContextState }}>
+      <Div />
+    </GlobalContext.Provider>
   );
 };
 
