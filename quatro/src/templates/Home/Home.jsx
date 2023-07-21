4ece4fafe7266d149a50bf469f1bbd067e6e2d64
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-21 12:03:07 -0300

    Cinco

--- quatro/src/templates/Home/Home.jsx
+++ quatro/src/templates/Home/Home.jsx
@@ -1,20 +1,57 @@
 /* eslint-disable no-unused-vars */
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+
+const useAsync = (asyncFuntion, shouldRun) => {
+  const [result, setResult] = useState(null);
+  const [state, setState] = useState({
+    result: null,
+    error: null,
+    status: 'pending',
+  });
+  const [status, setStatus] = useState('idle');
+
+  const run = useCallback(() => {
+    console.log('effect', new Date().toLocaleTimeString());
+    setState({ result: null, error: null, status: 'idle' });
+
+    return asyncFuntion()
+      .then((response) => {
+        setState({ result: response, error: null, status: 'settled' });
+      })
+      .catch((error) => {
+        setState({ result: null, error: error, status: 'error' });
+      });
+  }, [asyncFuntion]);
+
+  useEffect(() => {
+    if (shouldRun) {
+      run();
+    }
+  }, [run, shouldRun]);
+
+  return [run, state.result, state.error, state.status];
+};
+
+const fetchData = async () => {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts/');
+  return await data.json();
+};
 
 export const Home = () => {
   const [posts, setPosts] = useState(null);
+  const [reFetchData, result, error, status] = useAsync(fetchData, true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetch('https://jsonplaceholder.typicode.com/posts/');
-      const json = await data.json();
-      setPosts(json);
-      return json;
-    };
-
-    fetchData();
-  }, []);
-
-  return <pre>{JSON.stringify(posts, null, 2)}</pre>;
+  if (status === 'idle') {
+    return <pre>Nada Executando</pre>;
+  }
+  if (status === 'pending') {
+    return <pre>Loading...</pre>;
+  }
+  if (status === 'error') {
+    return <pre>{error.message}</pre>;
+  }
+  if (status === 'settled') {
+    return <pre>{JSON.stringify(result, null, 2)}</pre>;
+  }
+  return 'xiiiiiiiiiii';
 };
--- quatro/src/templates/Home/Home.test.jsx
+++ quatro/src/templates/Home/Home.test.jsx
@@ -1,33 +1,40 @@
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
-import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import { Home } from './Home';
 
 const hendlers = [
-  rest.get('https://jsonplaceholder.typicode.com/posts', async (req, res, ctx) => {
-    return res(
-      ctx.json([
-        {
-          userId: 1,
-          id: 1,
-          title: 'sunt aut',
-          body: 'quia et',
-        },
-        {
-          userId: 2,
-          id: 2,
-          title: 'qui est esse',
-          body: 'est reru',
-        },
-        {
-          userId: 3,
-          id: 3,
-          title: 'ea molestias',
-          body: 'et iusto',
-        },
-      ]),
-    );
-  }),
+  rest.get(
+    'https://jsonplaceholder.typicode.com/posts',
+    async (req, res, ctx) => {
+      return res(
+        ctx.json([
+          {
+            userId: 1,
+            id: 1,
+            title: 'sunt aut',
+            body: 'quia et',
+          },
+          {
+            userId: 2,
+            id: 2,
+            title: 'qui est esse',
+            body: 'est reru',
+          },
+          {
+            userId: 3,
+            id: 3,
+            title: 'ea molestias',
+            body: 'et iusto',
+          },
+        ]),
+      );
+    },
+  ),
 ];
 const server = setupServer(...hendlers);
 
