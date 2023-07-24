bfec8ad9f798c50b183fcd781e4cbcd2cd75fd83
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-24 11:30:12 -0300

    React.children + React.cloneElement

--- quatro/src/templates/Home/Home.jsx
+++ quatro/src/templates/Home/Home.jsx
@@ -1,48 +1,23 @@
-import { useDebugValue, useEffect, useState } from 'react';
+/* eslint-disable no-unused-vars */
+const s = {
+  style: {
+    fontSize: '60px',
+  },
+};
 
-const useMediaQuery = (queryValue, initialValue = false) => {
-  const [match, setMatch] = useState(initialValue);
+import { Children, cloneElement } from 'react';
 
-  useDebugValue(`Query: ${queryValue}`, (name) => {
-    return name + ' modificado';
+const Parent = ({ children }) => {
+  return Children.map(children, (child) => {
+    const newChild = cloneElement(child, { ...s });
+    return newChild;
   });
-
-  useEffect(() => {
-    let isMounted = true;
-    const matchMedia = window.matchMedia(queryValue);
-
-    const handleChange = () => {
-      if (!isMounted) return;
-      setMatch(Boolean(matchMedia.matches));
-    };
-
-    matchMedia.addEventListener('change', handleChange);
-    setMatch(!!matchMedia.matches);
-
-    return () => {
-      isMounted = false;
-      matchMedia.removeEventListener('change', handleChange);
-    };
-  }, [queryValue]);
-
-  return match;
 };
 
 export const Home = () => {
-  const huge = useMediaQuery('(min-width: 980px)');
-  const big = useMediaQuery('(max-width: 979px) and (min-width: 768px)');
-  const medium = useMediaQuery('(max-width: 767px) and (min-width: 321px)');
-  const small = useMediaQuery('(max-width: 321px)');
-
-  const background = huge
-    ? 'green'
-    : big
-    ? 'red'
-    : medium
-    ? 'yellow'
-    : small
-    ? 'purple'
-    : null;
-
-  return <div style={{ fontSize: '60px', background }}>Oi</div>;
+  return (
+    <Parent>
+      <p>Ola</p>
+    </Parent>
+  );
 };
