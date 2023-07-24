8c6e6f777b930c7511adf2761eb214d350c8d644
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-24 15:42:29 -0300

    Reorganização

--- dois/src/context/App/data.js
+++
@@ -1,5 +0,0 @@
-export const globalState = {
-  title: 'titulo do contexto',
-  counter: 0,
-  body: 'body do contexto',
-};
--- quatro/src/utils/mock.js
+++
@@ -1,6 +0,0 @@
-export const mock = {
-    title: 'title 1',
-    body: 'body 1',
-    id: 1,
-    cover: 'img/img.png'
-}
