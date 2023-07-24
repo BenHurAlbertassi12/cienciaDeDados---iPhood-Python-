0953af337c474b7188a8aa138d52ad551a5e280d
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-24 12:36:42 -0300

    React.children + React.cloneElement

--- quatro/src/templates/Home/Home.jsx
+++ quatro/src/templates/Home/Home.jsx
@@ -1,23 +1,45 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { Children, cloneElement, useState } from 'react';
+
 const s = {
   style: {
     fontSize: '60px',
   },
 };
 
-import { Children, cloneElement } from 'react';
+const TurnOnOff = ({ children }) => {
+  const [isOn, setIsOn] = useState(false);
+  const onTurn = () => setIsOn((s) => !s);
 
-const Parent = ({ children }) => {
   return Children.map(children, (child) => {
-    const newChild = cloneElement(child, { ...s });
+    const newChild = cloneElement(child, {
+      isOn,
+      onTurn,
+    });
     return newChild;
   });
 };
+const TurnedOn = ({ isOn, children }) => (isOn ? children : null);
+const TurnedOff = ({ isOn, children }) => (isOn ? null : children);
+const TurnButton = ({ isOn, onTurn, ...props }) => {
+  return (
+    <button onClick={onTurn} {...props}>
+      Turn {isOn ? 'OFF' : 'ON'}
+    </button>
+  );
+};
+const P = ({ children }) => <p {...s}>{children}</p>;
 
 export const Home = () => {
   return (
-    <Parent>
-      <p>Ola</p>
-    </Parent>
+    <TurnOnOff>
+      <TurnedOn>
+        <P>Aqui as coisas que vão acontecer quando estiver ON.</P>
+      </TurnedOn>
+      <TurnedOff>
+        <P>Aqui vem as coisas do OFF.</P>
+      </TurnedOff>
+      <TurnButton {...s} />
+    </TurnOnOff>
   );
 };
