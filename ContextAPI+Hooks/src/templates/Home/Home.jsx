00cd9fa918cb280bc2c4f0b1e0230db59bba3dd8
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-07-25 17:16:41 -0300

    Context API + Hooks

--- ContextAPI+Hooks/src/templates/Home/Home.jsx
+++ ContextAPI+Hooks/src/templates/Home/Home.jsx
@@ -1,24 +1,37 @@
-/* eslint-disable no-unused-vars */
-import { useEffect, useState } from 'react';
-import { useCounterContext } from '../../context/CounterContext/context';
-import { Heading } from '../../components/Heading/Heading';
 import { Button } from '../../components/Button/Button';
+import { Heading } from '../../components/Heading/Heading';
+import { useCounterContext } from '../../context/CounterContext/context';
 
 export const Home = () => {
   const [state, actions] = useCounterContext();
 
-  useEffect(() => {
-    actions.increase();
-  }, [actions]);
+  const handleError = () => {
+    actions
+      .asyncError()
+      .then((r) => console.log(r))
+      .catch((e) => console.log(e.name, ':', e.message));
+  };
 
   return (
     <div>
-      <h1 onClick={() => actions.increase()}>Oi</h1>
-
       <Heading />
 
       <div>
-        <Button onButtonClick={actions.increase}>Increase</Button>
+        <Button onButtonClick={actions.increase}>increase</Button>
+        <Button onButtonClick={actions.decrease}>decrease</Button>
+        <Button onButtonClick={actions.reset}>reset</Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 10 })}>
+          set 10
+        </Button>
+        <Button onButtonClick={() => actions.setCounter({ counter: 100 })}>
+          set 100
+        </Button>
+        <Button disabled={state.loading} onButtonClick={actions.asyncIncrease}>
+          async increase
+        </Button>
+        <Button disabled={state.loading} onButtonClick={handleError}>
+          async error
+        </Button>
       </div>
     </div>
   );
