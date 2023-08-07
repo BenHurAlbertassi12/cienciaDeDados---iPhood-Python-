f6cfd560bc59e3feadfafa69cf85544bcef2c657
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-08-07 15:50:05 -0300

    .

--- RouterDomV5/src/index.js
+++ RouterDomV5/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Home } from './templates/Home/Home';
+import { Abc } from './templates/abc';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/abc" component={Abc}></Route>
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
