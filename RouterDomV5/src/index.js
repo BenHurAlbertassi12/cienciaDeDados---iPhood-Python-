d1295e2a578bb7b473a01244b23ebb196987100b
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-08-07 15:43:24 -0300

    .

--- RouterDomV5/src/index.js
+++ RouterDomV5/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 import { Home } from './templates/Home/Home';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>,
+  document.getElementById('root'),
 );
-
-reportWebVitals();
