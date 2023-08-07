e7550bd1506d4a801ab4a579c30cf2ef38624851
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-08-07 16:44:45 -0300

    .

--- RouterDomV5/src/templates/abc/index.jsx
+++ RouterDomV5/src/templates/abc/index.jsx
@@ -1,12 +1,27 @@
-import { useParams } from 'react-router-dom/';
+import { useEffect } from 'react';
+import { useParams, useLocation, useHistory } from 'react-router-dom/';
 
 export const Abc = () => {
   const { slug, id } = useParams();
+  const location = useLocation();
+  const history = useHistory();
+
+  useEffect(() => {
+    setTimeout(function () {
+      history.push('/');
+    }, 1000);
+  }, [history]);
+
+  console.log(location, history);
 
   return (
     <div>
       <h1>
-        ABC{slug} {id}
+        ABC
+        {slug}
+        {id}
+        {/* {location} */}
+        {/* {history} */}
       </h1>
     </div>
   );
