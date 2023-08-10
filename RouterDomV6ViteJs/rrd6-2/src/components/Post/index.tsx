d0e587a720bdd7556e6d8e82ce9ae6e1e73eee3a
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-08-10 15:44:54 -0300

    id

--- RouterDomV6ViteJs/rrd6-2/src/components/Post/index.tsx
+++ RouterDomV6ViteJs/rrd6-2/src/components/Post/index.tsx
@@ -1,8 +1,14 @@
+import {useParams} from 'react-router-dom'
+
 export const Post = () => {
+  const params = useParams();
+  const {id} = params
+  
+
   return (
     <div>
       <h1>
-        post
+        post {id}
       </h1>
     </div>
   )
